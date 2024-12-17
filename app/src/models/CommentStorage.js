41df02f748c4c332d76d5d6c0e8981f99fbbc86f
Channeeee <162098285+Channeeee@users.noreply.github.com>
2024-12-17 21:44:20 +0900

    commentStorage 수정

--- app/src/models/CommentStorage.js
+++ app/src/models/CommentStorage.js
@@ -1,16 +1,16 @@
 const db = require("../config/db");
 
 class CommentStorage {
+    // 댓글 저장 메서드
     static saveComment(comments) {
         return new Promise((resolve, reject) => {
             const query = `
                 INSERT INTO comments (comment, postnum, user_id, date, parent_id, ref)
-                VALUES (?, ?, ?, ?, ?, ?)`;
+                VALUES (?, ?, ?, NOW(), ?, ?)`;
             db.query(query, [
                 comments.comment,
                 comments.postnum,
                 comments.user_id,
-                comments.date,
                 comments.parent_id,
                 comments.ref,
             ], (err, result) => {
@@ -20,34 +20,55 @@ class CommentStorage {
         });
     }
 
+    // 특정 게시글의 댓글 가져오기
     static getCommentsByPostId(postnum) {
         return new Promise((resolve, reject) => {
             const query = `
-                SELECT * FROM comments 
-                WHERE postnum = ? 
-                ORDER BY IF(parent_id = 0, commentsID, parent_id), date ASC
+                SELECT commentsID, postnum, user_id, parent_id, comment, date, ref, report
+                FROM comments
+                WHERE postnum = ?
+                ORDER BY IF(parent_id IS NULL, commentsID, parent_id), date ASC
             `;
+
             db.query(query, [postnum], (err, results) => {
-                if (err) reject(`${err}`);
-                // report가 1인 댓글 처리
-            const processedComments = results.map((comment) => {
-                if (comment.report === 1) {
-                    comment.comment = "차단된 댓글입니다";
+                if (err) {
+                    console.error("댓글 조회 오류:", err);
+                    return reject(err);
                 }
-                return comment;
+
+                // report가 1인 댓글 처리
+                const processedComments = results.map((comment) => {
+                    if (comment.report === 1) {
+                        return {
+                            ...comment,
+                            comment: "차단된 댓글입니다", // comment 내용 차단 메시지로 변경
+                        };
+                    }
+                    return comment;
+                });
+
+                resolve(processedComments);
             });
-
-            resolve(processedComments);
         });
-    });
-}    
+    }
 
+    // 특정 댓글 가져오기
     static getCommentById(commentsID) {
         return new Promise((resolve, reject) => {
-            const query = "SELECT * FROM comments WHERE commentsID = ?";
+            const query = `
+                SELECT commentsID, postnum, user_id, parent_id, comment, date, ref, report
+                FROM comments 
+                WHERE commentsID = ?`;
+
             db.query(query, [commentsID], (err, result) => {
-                if (err) reject(`${err}`);
-                resolve(result[0]);
+                if (err) return reject(err);
+
+                const comment = result[0];
+                if (comment && comment.report === 1) {
+                    comment.comment = "차단된 댓글입니다"; // report가 1인 경우 차단된 댓글 처리
+                }
+
+                resolve(comment);
             });
         });
     }
